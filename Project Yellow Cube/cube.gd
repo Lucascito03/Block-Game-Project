98bd69420f689196ab563bd7824157855d105e1b
zenith391 <39484230+zenith391@users.noreply.github.com>
2022-09-24 19:05:41 +0200

    Add object selection

--- Project Yellow Cube/cube.gd
+++ Project Yellow Cube/cube.gd
@@ -1,15 +1,31 @@
 extends RigidBody
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var camera # the camera of the scene
+var mesh # the mesh of this object
+var original_color # the color that the mesh of this object has by default
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	camera = get_node("/root/Spatial/Camera")
+	mesh = get_node("Mesh")
+	original_color = mesh.material.albedo_color
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		# Cast an imaginary 'ray' from the camera to where the mouse point
+		# This is used to know which object the mouse selects
+		var ray_length = 100;
+		var from = camera.project_ray_origin(event.position);
+		var to = from + camera.project_ray_normal(event.position) * ray_length;
+		var space_state = get_world().direct_space_state;
+		var result = space_state.intersect_ray(from, to)
+		
+		# Do a selection box around only if it's been hit by the ray
+		mesh.material.albedo_color = original_color
+		if result.size() != 0: # if the ray has collided.
+			var collider = result.collider
+			if collider == self:
+				mesh.material.albedo_color = original_color.darkened(0.2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
